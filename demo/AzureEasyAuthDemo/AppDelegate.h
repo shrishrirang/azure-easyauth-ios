b678f275c99fa91afe0be9aa8c73c8433da1a593
Shrirang Shirodkar <shrirs@microsoft.com>
2017-02-02 10:35:49 -0800

    Add empty demo project and a workspace
    - Demo project is empty at this stage. Follow up commits will add code to it.
    - Workspace contains source and demo Xcode projects.

---
+++ demo/AzureEasyAuthDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AzureEasyAuthDemo
+//
+//  Created by Shrirang on 2/2/17.
+//  Copyright © 2017 Microsoft Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/AzureEasyAuthDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AzureEasyAuthDemo
+//
+//  Created by Shrirang on 2/2/17.
+//  Copyright © 2017 Microsoft Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
