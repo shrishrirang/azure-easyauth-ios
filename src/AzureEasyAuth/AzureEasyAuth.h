2967061bbfa01958b2e253474b92b34d7fc6e939
Shrirang Shirodkar <shrirs@microsoft.com>
2017-02-02 11:37:09 -0800

    Update umbrella header, project settings and demo app

--- src/AzureEasyAuth/AzureEasyAuth.h
+++ src/AzureEasyAuth/AzureEasyAuth.h
@@ -16,4 +16,8 @@ FOUNDATION_EXPORT const unsigned char AzureEasyAuthVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AzureEasyAuth/PublicHeader.h>
 
+#import "MSBlockDefinitions.h"
+#import "MSError.h"
+#import "MSLoginSafariViewController.h"
+#import "MSUser.h"
 
