2cb1bf5e5c7ddd4a77fcb26354452ed680d11993
Shrirang Shirodkar <shrirs@microsoft.com>
2017-02-02 15:51:15 -0800

    Update demo app to demonstrate login flow

--- demo/AzureEasyAuthDemo/AppDelegate.h
+++ demo/AzureEasyAuthDemo/AppDelegate.h
@@ -7,11 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AzureEasyAuth/AzureEasyAuth.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
+@property (strong, nonatomic) MSLoginSafariViewController* loginController;
 
 @end
 
--- demo/AzureEasyAuthDemo/ViewController.m
+++ demo/AzureEasyAuthDemo/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "AzureEasyAuth/AzureEasyAuth.h"
+#import "AppDelegate.h"
 
 @interface ViewController ()
 
@@ -18,6 +19,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    AppDelegate* delegate = (AppDelegate*) [[UIApplication sharedApplication] delegate];
+    
+    delegate.loginController = [[MSLoginSafariViewController alloc] initWithBackendUrl:@"https://replace_this_host_name.azurewebsites.net"];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [delegate.loginController loginWithProvider:@"google" urlScheme:@"demoscheme" parameters:nil controller:self animated:NO completion:^(MSUser * _Nullable user, NSError * _Nullable error) {
+            if (user) {
+                NSLog(@"User: %@", user.userId);
+            } else {
+                NSLog(@"Error: %@", [error description]);
+            }
+        }];
+    });
+    
+    
 }
 
 
