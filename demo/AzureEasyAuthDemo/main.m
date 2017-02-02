474770307d23b4a975187d9f10bf3bc8b2fdb1b3
Shrirang Shirodkar <shrirs@microsoft.com>
2017-02-02 10:35:49 -0800

    Add empty demo project and a workspace
    - Demo project is empty at this stage. Follow up commits will add code to it.
    - Workspace contains source and demo Xcode projects.

---
+++ demo/AzureEasyAuthDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AzureEasyAuthDemo
+//
+//  Created by Shrirang on 2/2/17.
+//  Copyright © 2017 Microsoft Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
